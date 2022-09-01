ec64dd66a548a072814f31c4418484ab585696ab
eugenioclrc <eugenioclrc@gmail.com>
2022-09-01 15:39:34 -0300

    link demo

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -13,7 +13,7 @@ $: extraHeaderClass = !$ghostHeader ? 'bg-base-100 text-base-content shadow-sm' 
     <div class="navbar-center hidden lg:flex">
       <ul class="menu menu-horizontal p-0">
         <li tabindex="0">
-          <a>
+          <a href="/pages-hello-world">
             Primeros pasos
             <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
           </a>
