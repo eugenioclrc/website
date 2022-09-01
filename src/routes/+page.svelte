0f409bae6705f4df2a746641b11adcb4e597b844
eugenioclrc <eugenioclrc@gmail.com>
2022-08-31 22:32:04 -0300

    webtr3s fork

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<svelte:head>
+	<title>WEBTR3S CLUB</title>
+</svelte:head>
+
+<div class="from-indigo-700 to-black text-primary-content -mt-[4rem] grid place-items-center items-end bg-gradient-to-br pt-20">
+	<div class="hero min-h-screen /*bg-base-200*/">
+		<div class="hero-content text-center">
+			<div class="max-w-4xl">
+				<h2 class="py-2 text-2xl md:text-4xl">Desarrollo && Web3</h2>
+				<h1 class="py-4 text-4xl md:text-7xl font-bold">
+					Bienvenido a <a href="https://discord.com/invite/eegRCDmwbM" class="link-secondary" target="_blank">WebtrES</a>
+				</h1>
+				<p class="py-6 text-xl md:text-2xl tracking-wide leading-relaxed">
+					Somos una comunidad hispanohablante apasionada por el desarrollo Web 3.0 integrada por desarrolladores de diferentes ramas, todos contibuyendo a que el aprendizaje sea mas ameno y divertido en conjunto
+				</p>
+				<a href="https://discord.com/invite/eegRCDmwbM" target="_blank" class="btn btn-xl btn-primary">Unirme en Discord</a>
+				
+			</div>
+		</div>
+	</div>
+</div>
